205573901f390b6503d6bc2ecd7bc591648e8355
Lauren Hale <lmhale710@gmail.com>
2017-11-27 19:25:47 -0600

    Adding hale-assignment1.4.js

---
+++ week-1/hale-assignment1.4.js
@@ -0,0 +1,33 @@
+// start program
+/*
+Expected output:
+
+Lauren
+Hale
+24
+Chestnut Ave
+Tulsa
+Oklahoma
+74011
+*/
+
+// variable declaration and assignment goes here...
+var firstName = "Lauren";
+var lastName = "Hale";
+var age = "24";
+var street = "Chestnut Ave";
+var city = "Tulsa";
+var state = "Oklahoma";
+var zipCode = "74011";
+
+// output
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(state);
+console.log(zipCode);
+
+//end program
+
