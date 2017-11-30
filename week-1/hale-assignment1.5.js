ee1a23f201e1f9cb46900e4f17f8bdf6c2fbaf2f
Lauren Hale <lmhale710@gmail.com>
2017-11-30 14:07:14 -0600

    Adding hale-assignment1.5.js

---
+++ week-1/hale-assignment1.5.js
@@ -0,0 +1,68 @@
+
+
+var myFirstName = "Lauren";
+var myLastName = "Hale";
+var todaysDate = new Date().toLocaleDateString();
+var assignmentNum = "Assignment 1.5"
+
+var programHeader = "\n" + myFirstName + " " + myLastName + "\n" +
+assignmentNum +"\nDate:" + todaysDate;
+
+console.log(programHeader);
+
+var firstName1 = "Mandy";
+var lastName1 = "Moore";
+var Address1 = "5555 Melrose Avenue Los Angeles, CA";
+var payRate1 = 18.75;
+payRate1.toFixed(1);
+var hireDate1 = new Date("01/05/2018").toLocaleDateString();
+
+
+
+console.log("\n" + firstName1 + " " + lastName1 + "\nAddress:" +
+Address1 +"\nPay Rate:$" + payRate1 +"\nHire Date:"+ hireDate1 );
+
+var firstName2 = "Ryan";
+var lastName2 = "Gosling";
+var Address2 = "5555 Melrose Avenue Los Angeles, CA";
+var payRate2 = 20.50;
+payRate2.toFixed(2);
+var hireDate2 = new Date("01/10/2018").toLocaleDateString();
+
+console.log("\n" + firstName2 + " " + lastName2 + "\nAddress:" + Address2 + "\nPay Rate:$" +payRate2 + "\nHire Date:" + hireDate1);
+
+var firstName3 = "Emma";
+var lastName3 = "Stone";
+var Address3 = "3532 Hayden Avenue Los Angeles, CA";
+var payRate3 = 19.50;
+payRate3.toFixed(2);
+var hireDate3 = new Date("01/15/2018").toLocaleDateString();
+
+console.log("\n" + firstName3 + " " + lastName3 + "\nAddress:" + Address3 + "\nPay Rate:$" +payRate3 + "\nHire Date:" + hireDate3);
+
+
+var firstName4 = "Jack";
+var lastName4 = "Black";
+var Address4 = "3727 W. Magnolia Burbank, CA";
+var payRate4 = 17.25;
+payRate4.toFixed(2);
+var hireDate4 = new Date("01/25/2018").toLocaleDateString();
+
+console.log("\n" + firstName4 + " " + lastName4 + "\nAddress:" + Address4 + "\nPay Rate:$" +payRate4 + "\nHire Date:" + hireDate4);
+
+var firstName5 = "Anna";
+var lastName5 = "Kendrick";
+var Address5 = "8436 W. Third Street Los Angeles, CA.";
+var payRate5 = 19.00;
+payRate4.toFixed(0);
+var hireDate5 = new Date("01/27/2018").toLocaleDateString();
+
+
+console.log("\n" + firstName5 + " " + lastName5 + "\nAddress:" + Address5 + "\nPay Rate:$" +payRate5 + "\nHire Date:" + hireDate5);
+
+
+
+
+
+
+
