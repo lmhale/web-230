f62ada3c02b24fde00c3e2b8a7fad5a4a46b1b6d
Lauren Hale <lmhale710@gmail.com>
2017-12-04 14:49:38 -0600

    Adding assignment2.3.js

---
+++ week-2/hale-assignment2.3.js
@@ -0,0 +1,13 @@
+var header = require('../header.js');
+
+console.log(header.display("Lauren", "Hale","Exercise 2.3"));;
+
+
+var myName = "Lauren Hale";
+
+function myName(){
+   
+}
+
+    
+console.log("\n" + "Hello"+" " + myName +"!");
