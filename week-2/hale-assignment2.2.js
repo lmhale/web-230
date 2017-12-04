91ca5714dd1a9bed33e6df7f14ccb9671bfc80a9
Lauren Hale <lmhale710@gmail.com>
2017-12-04 12:45:16 -0600

    Adding Assignment 2.2.js

---
+++ week-2/hale-assignment2.2.js
@@ -0,0 +1,6 @@
+var header = require('../header.js');
+console.log("\ntesting the header.js file's output...");
+
+console.log(header.display("Richard", "Krasso","Exercise 2.2"));
+
+console.log("\n");
