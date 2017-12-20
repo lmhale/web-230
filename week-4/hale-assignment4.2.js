89fe61da0594821dc7aed5c8f4491f7720173a7c
Lauren Hale <lmhale710@gmail.com>
2017-12-19 23:42:56 -0600

    adding assign 4.2

---
+++ week-4/hale-assignment4.2.js
@@ -0,0 +1,14 @@
+var header = require('../header.js');
+
+console.log(header.display("Lauren", "Hale","Exercise 4.2"));
+
+var fruits = ["Apple","Orange","Banana","Mango","Pear"];
+
+
+function getFruit(f) {
+
+    console.log(fruits);
+    
+}
+
+getFruit();
