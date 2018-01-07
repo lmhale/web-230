c1d2e35ac318fddba6c8326596633c42817c8c3b
Lauren Hale <lmhale710@gmail.com>
2018-01-06 18:45:46 -0600

    Changing 3.2 and 3.4

--- week-3/hale-assign3.2.js
+++ week-3/hale-assign3.2.js
@@ -2,6 +2,11 @@ var header = require('../header.js');
 
 console.log(header.display("Lauren", "Hale","Exercise 3.2"));
 
+
+var And = " and ";
+var MATCH = " do match!";
+var MISMATCH = " do not match!";
+
 function match (argument1,argument2) {
     if (argument1 === argument2)
     return true;
@@ -9,4 +14,45 @@ function match (argument1,argument2) {
         return false;
     }
 }
-console.log("cat","cat");
+
+
+function logMismatch( argument1, argument2) {
+    var logMismatchMessage = argument1 + And + argument2 + MISMATCH;
+    console.log(logMismatchMessage);
+}
+
+function logMatch ( argument1, argument2 ) {
+    var logMatchMessage = argument1 + And + argument2 + MATCH;
+    console.log(logMatchMessage);
+}
+
+
+var one = "Truck";
+var two = "Car";
+var three = "Bike";
+var four = "Bike";
+var five = "Three";
+var six = "Four";
+
+
+console.log(match(one,two));
+console.log(match(three,four));
+
+if (match(one, two)) {
+    logMatch(one, two);
+} else {
+    logMismatch(one, two);
+}
+
+if (match(three, four)) {
+    logMatch(three, four);
+} else {
+    logMismatch(three, four);
+}
+
+if (match(five, six)) {
+    logMatch(five, six);
+} else {
+    logMismatch(five, six);
+}
+
--- week-3/hale-assign3.4.js
+++ week-3/hale-assign3.4.js
@@ -1,24 +1,76 @@
 var header = require('../header.js');
 
-console.log(header.display("Lauren", "Hale","Exercise 3.2"));
+console.log(header.display("Lauren", "Hale","Exercise 3.4"));
+
+// test variable
+var test = 4;
+var num =randomNumber();
+var And = " and ";
+var MATCH = " do match!";
+var MISMATCH = " do not match!";
 
 
-
-for (x=1;x<10;x++){
-    console.log(x,randomNumber());
+// for loop
+console.log("-- Displaying for loop --");
+for(i=0;i<10;i++){
+    if( match(i, num) ){
+        logMatch(i, num)
+    }else{
+        logMismatch(i, num)
+    }
 }
 
 
-var y = 3;
 
-while (y<10){
-console.log(y,randomNumber());
-y++
+
+
+// while loop
+console.log("\n-- Displaying while loop --"); 
+
+var test2 = 0;
+while (test2 < 10){
+    
+    if( match(test2, num) ){
+        logMatch(test2, num)
+    }else{
+        logMismatch(test2, num)
+    }
+    test2++;
+}
+
+
+// Reused functions from exercise-3.2.js
+
+
+function match (argument1,argument2) {
+    if (argument1 === argument2)
+    return true;
+    else {
+        return false;
+    }
+}
+
+
+function logMismatch( argument1, argument2) {
+    var logMismatchMessage = argument1 + And + argument2 + MISMATCH;
+    console.log(logMismatchMessage);
+}
+
+function logMatch ( argument1, argument2 ) {
+    var logMatchMessage = argument1 + And + argument2 + MATCH;
+    console.log(logMatchMessage);
 }
 
 
 
 
+
+// end program 
+
+
+// ** DO NOT MODIFY OR REMOVE THIS FUNCTION **
 function randomNumber() {
     return Math.floor((Math.random() * 10) + 1); 
 }
+
+
