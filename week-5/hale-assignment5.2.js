b6cd92b789f8d90c811093404ed48e2a28bd6f52
Lauren Hale <lmhale710@gmail.com>
2018-01-09 13:26:46 -0600

    adding assign 5.2

---
+++ week-5/hale-assignment5.2.js
@@ -0,0 +1,10 @@
+
+var header = require('../header.js');
+
+console.log(header.display("Lauren", "Hale","Exercise 5.2"));
+
+
+//itertes over each element in the array 
+var food = ["ice cream","sushi","steak","burgers","pizza"];
+
+food.forEach(function(food) {console.log(food)});
